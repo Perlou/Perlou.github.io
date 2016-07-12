76597552bece641f9f320ef6f761af01cf0419eb
Perlou <a87387875@163.com>
2016-07-12 12:18:50 +0800

    rebuild

--- js/json.js
+++ js/json.js
@@ -39,70 +39,20 @@ var Json = {
             codeSrc: ''
         },
         {
-            name:'Voxel',
-            date:'个人项目',
-            detail:'前端关键技术: javascript,canvas,css3新特性,webpack,yeoman.<br/>运用了较多css3和html5新特性的全屏滚动切换网站,基于原生javascript编写,yeoman作为构建工具,webpack作为打包工具进行优化和合理加载.使用了css3的3d场景特性,mousewheel,canvas,三角函数,css3动画等技术.',
-            image:['images/photo/5/1.png','images/photo/5/2.png','images/photo/5/3.png','images/photo/5/4.png'],
-            link:'perlou.github.io/Voxel',
-            codeSrc: 'github.com/Perlou/Voxel'
-        },
-        {
-            name:'移动端打怪小游戏',
-            date:'个人项目',
-            detail:'前端关键技术: 移动端自适应布局,css3动画,javascript,touch事件,canvas,webpack,yeoman.<br/>一个应用在微信场景中的移动端canvas小游戏,基于原生javascript编写,yeoman作为构建工具,webpack作为打包工具进行优化和合理加载,jCanvaScript作为canvas动画库.使用了移动端touch事件来切换场景和完成怪物的点击,css3的transition过渡动画,canvas作为此游戏的核心.',
-            image:['images/photo/6/1.png','images/photo/6/2.png','images/photo/6/3.png','images/photo/6/4.png'],
-            link:'perlou.github.io/MonsterGame',
-            codeSrc: 'github.com/Perlou/MonsterGame',
-            ecode:'images/photo/6/ecode.png'
-        },
-        {
-            name:'Diagnosite',
-            date:'个人项目',
-            detail:'前端关键技术: TweenMax动画库,jQuery,响应式布局,jQuery相关插件,webpack,yeoman.<br/>运用了较多css3和html5新特性的全屏滚动切换网站,基于jQuery编写,yeoman作为构建工具,webpack作为打包工具进行优化和合理加载.使用了css3的3d场景特性,mousewheel,canvas,三角函数,css3动画等技术.',
-            image:['images/photo/7/1.png','images/photo/7/2.png','images/photo/7/3.png','images/photo/7/4.png'],
-            link:'perlou.github.io/Diagnosite',
-            codeSrc: 'github.com/Perlou/Diagnosite'
-        },
-        {
             name:'贵州电子收费网站',
             date:'公司项目',
             detail:'前端关键技术: jQuery,html,css,jQuery相关插件,Ajax.<br/>负责该网站的静态页面,js前端交换,js动画效果,Ajax前后台数据交互功能和后期维护等前端开发工作.',
-            image:['images/photo/8/1.png','images/photo/8/2.png','images/photo/8/3.png','images/photo/8/4.png'],
+            image:['images/photo/5/1.png','images/photo/5/2.png','images/photo/5/3.png','images/photo/5/4.png'],
             link:'www.gzetc12122.cn',
             codeSrc: ''
-        },
-        {
-            name:'100度享乐网',
-            date:'个人项目',
-            detail:'前端关键技术: jQuery,html,css.<br/>100度享乐网仿站,兼容低版本IE.',
-            image:['images/photo/9/1.png','images/photo/9/2.png','images/photo/9/3.png','images/photo/9/4.png'],
-            link:'perlou.github.io/100du',
-            codeSrc: 'github.com/Perlou/100du'
         },
         {
             name:'中国高速公路运营管理网',
             date:'公司项目',
             detail:'前端关键技术: jQuery,html,css,jQuery相关插件,Ajax.<br/>负责该网站的静态页面,js前端交换,js动画效果,Ajax前后台数据交互功能和后期维护等前端开发工作.',
-            image:['images/photo/10/1.png','images/photo/10/2.png','images/photo/10/3.png','images/photo/10/4.png'],
+            image:['images/photo/6/1.png','images/photo/6/2.png','images/photo/6/3.png','images/photo/6/4.png'],
             link:'www.tollroad.com.cn',
             codeSrc: ''
-        },
-        {
-            name:'React图片画廊',
-            date:'个人项目',
-            detail:'前端关键技术: ReactJs,css3新特性,webpack,yeoman.<br/>一个基于React和css3开发的图片画廊应用,yeoman的generator-react-webpack作为脚手架,webpack作为打包工具进行优化和合理加载.',
-            image:['images/photo/11/1.png','images/photo/11/2.png','images/photo/11/3.png','images/photo/11/4.png'],
-            link:'perlou.github.io/gallery-by-react',
-            codeSrc: 'github.com/Perlou/gallery-by-react'
-        },
-        {
-            name:'移动端3D场景',
-            date:'个人项目',
-            detail:'前端关键技术: rem自适应布局,css3d特性,javascript,touch事件,webpack,yeoman.<br/>一个基于css3的3d场景开发的移动端小应用, 使用了自己封装的touch事件库, webpack作为打包工具进行优化和合理加载',
-            image:['images/photo/12/1.png','images/photo/12/2.png','images/photo/12/3.png','images/photo/12/4.png'],
-            link:'perlou.github.io/ThreeDChange',
-            codeSrc: 'github.com/Perlou/ThreeDChange',
-            ecode:'images/photo/12/ecode.png'
         }
     ],
     work: [
