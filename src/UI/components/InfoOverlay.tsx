c4b4579e3c3cc9a8d89a1f93da7f759a17940b2f
Perlou <perloukevin@gmail.com>
2022-05-14 00:56:22 +0800

    feat: github link

--- src/UI/components/InfoOverlay.tsx
+++ src/UI/components/InfoOverlay.tsx
@@ -6,7 +6,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import FreeCamToggle from './FreeCamToggle'
 import MuteToggle from './MuteToggle'
-
+import { motion } from 'framer-motion'
+import github from '../../../static/textures/UI/github.svg'
 interface InfoOverlayProps {
     visible: boolean
 }
@@ -26,6 +27,7 @@ const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {
     const [textDone, setTextDone] = useState(false)
     const [volumeVisible, setVolumeVisible] = useState(false)
     const [freeCamVisible, setFreeCamVisible] = useState(false)
+    const [githubVisible, setGithubVisible] = useState(false)
 
     const typeText = (
         i: number,
@@ -89,6 +91,9 @@ const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {
                 setVolumeVisible(true)
                 setTimeout(() => {
                     setFreeCamVisible(true)
+                    setTimeout(() => {
+                        setGithubVisible(true)
+                    }, 250)
                 }, 250)
             }, 250)
         }
@@ -96,7 +101,7 @@ const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {
 
     useEffect(() => {
         window.postMessage({ type: 'keydown', key: `_AUTO_` }, '*')
-    }, [freeCamVisible, volumeVisible])
+    }, [githubVisible,freeCamVisible, volumeVisible])
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -143,13 +148,74 @@ const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {
                             <FreeCamToggle />
                         </div>
                     )}
+                    {githubVisible && (
+                        <div style={styles.lastRowChild}>
+                            <div
+                                style={styles.icon}
+                                className="icon-control-container"
+                                id="github-click"
+                                onClick={() => {
+                                    window.open('https://github.com/Perlou', '__blank')
+                                }}
+                            >
+                                <motion.img
+                                    id="prevent-click"
+                                    src={github}
+                                    height={26.5}
+                                    width={26.5}
+                                    variants={iconVars}
+                                />
+                            </div>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
     )
 }
 
+const iconVars = {
+    // hovering: {
+    //     // scale: 1.2,
+    //     opacity: 0.8,
+    //     transition: {
+    //         duration: 0.1,
+    //         ease: 'easeOut',
+    //     },
+    // },
+    // active: {
+    //     scale: 0.8,
+    //     opacity: 0.5,
+    //     transition: {
+    //         duration: 0.1,
+    //         ease: Easing.expOut,
+    //     },
+    // },
+    default: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            duration: 0.2,
+            ease: 'easeOut',
+        },
+    },
+}
+
+
 const styles: StyleSheetCSS = {
+    icon: {
+        background: 'white',
+        // padding: 4,
+        paddingLeft: 8,
+        paddingRight: 8,
+        textAlign: 'center',
+        display: 'flex',
+        // position: 'absolute',
+        boxSizing: 'border-box',
+        justifyContent: 'center',
+        alignItems: 'center',
+        cursor: 'pointer',
+    },
     container: {
         background: 'black',
         padding: 4,
